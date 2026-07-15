1f544152d400635a88dfce994fedd74e100d4f99
KARTHIKEYAN S <165674118+Karthikeyan1081@users.noreply.github.com>
2026-07-15 15:40:26 +0530

    Updated Last contest in Codeforces

--- src/main/java/com/track/track/dto/CodeforcesStatsDto.java
+++ src/main/java/com/track/track/dto/CodeforcesStatsDto.java
@@ -9,65 +9,36 @@ public class CodeforcesStatsDto {
     private String contestsAttended;
     private String lastActive;
 
-    public CodeforcesStatsDto() {
-    }
+    // Last contest info
+    private String lastContestName;
+    private String lastContestDate;
+    private String lastContestRank;
+    private String lastContestSolved;
+    private String lastContestFinished;
 
-    public CodeforcesStatsDto(String currentRating, String highestRating,
-                              String globalRank, String totalSolved,
-                              String contestsAttended, String lastActive) {
-        this.currentRating = currentRating;
-        this.highestRating = highestRating;
-        this.globalRank = globalRank;
-        this.totalSolved = totalSolved;
-        this.contestsAttended = contestsAttended;
-        this.lastActive = lastActive;
-    }
+    public CodeforcesStatsDto() {}
 
-    public String getCurrentRating() {
-        return currentRating;
-    }
+    public String getCurrentRating()    { return currentRating; }
+    public void setCurrentRating(String v)    { this.currentRating = v; }
+    public String getHighestRating()    { return highestRating; }
+    public void setHighestRating(String v)    { this.highestRating = v; }
+    public String getGlobalRank()       { return globalRank; }
+    public void setGlobalRank(String v)       { this.globalRank = v; }
+    public String getTotalSolved()      { return totalSolved; }
+    public void setTotalSolved(String v)      { this.totalSolved = v; }
+    public String getContestsAttended() { return contestsAttended; }
+    public void setContestsAttended(String v) { this.contestsAttended = v; }
+    public String getLastActive()       { return lastActive; }
+    public void setLastActive(String v)       { this.lastActive = v; }
 
-    public void setCurrentRating(String currentRating) {
-        this.currentRating = currentRating;
-    }
-
-    public String getHighestRating() {
-        return highestRating;
-    }
-
-    public void setHighestRating(String highestRating) {
-        this.highestRating = highestRating;
-    }
-
-    public String getGlobalRank() {
-        return globalRank;
-    }
-
-    public void setGlobalRank(String globalRank) {
-        this.globalRank = globalRank;
-    }
-
-    public String getTotalSolved() {
-        return totalSolved;
-    }
-
-    public void setTotalSolved(String totalSolved) {
-        this.totalSolved = totalSolved;
-    }
-
-    public String getContestsAttended() {
-        return contestsAttended;
-    }
-
-    public void setContestsAttended(String contestsAttended) {
-        this.contestsAttended = contestsAttended;
-    }
-
-    public String getLastActive() {
-        return lastActive;
-    }
-
-    public void setLastActive(String lastActive) {
-        this.lastActive = lastActive;
-    }
+    public String getLastContestName()     { return lastContestName; }
+    public void setLastContestName(String v)     { this.lastContestName = v; }
+    public String getLastContestDate()     { return lastContestDate; }
+    public void setLastContestDate(String v)     { this.lastContestDate = v; }
+    public String getLastContestRank()     { return lastContestRank; }
+    public void setLastContestRank(String v)     { this.lastContestRank = v; }
+    public String getLastContestSolved()   { return lastContestSolved; }
+    public void setLastContestSolved(String v)   { this.lastContestSolved = v; }
+    public String getLastContestFinished() { return lastContestFinished; }
+    public void setLastContestFinished(String v) { this.lastContestFinished = v; }
 }
