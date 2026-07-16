f9c773ff57600ebfe3c64b26892d864edbc57380
KARTHIKEYAN S <165674118+Karthikeyan1081@users.noreply.github.com>
2026-07-16 15:42:14 +0530

    Update CodeChefController.java

--- src/main/java/com/track/track/controller/CodeChefController.java
+++ src/main/java/com/track/track/controller/CodeChefController.java
@@ -2,6 +2,7 @@ package com.track.track.controller;
 
 import com.track.track.dto.CodeChefStatsDto;
 import com.track.track.service.CodeChefService;
+import com.track.track.service.LastContestService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.*;
 public class CodeChefController {
 
     private final CodeChefService codeChefService;
+    private final LastContestService lastContestService;
 
-    public CodeChefController(CodeChefService codeChefService) {
+    public CodeChefController(CodeChefService codeChefService,
+                               LastContestService lastContestService) {
         this.codeChefService = codeChefService;
+        this.lastContestService = lastContestService;
     }
 
     @GetMapping("/{username}")
     public CodeChefStatsDto getCodeChefStats(@PathVariable String username) {
-        return codeChefService.getCodeChefStats(username);
+        CodeChefStatsDto dto = codeChefService.getCodeChefStats(username);
+        lastContestService.fetchCodeChefLastContest(username, dto);
+        return dto;
     }
 }
