ce4cde8c494cb7e85dec31715bdc9de063609e03
KARTHIKEYAN S <165674118+Karthikeyan1081@users.noreply.github.com>
2026-07-16 15:41:52 +0530

    Integrate LastContestService in LeetCodeController
    Added LastContestService to fetch last contest details.

--- src/main/java/com/track/track/controller/LeetCodeController.java
+++ src/main/java/com/track/track/controller/LeetCodeController.java
@@ -2,6 +2,7 @@ package com.track.track.controller;
 
 import com.track.track.dto.LeetCodeStatsDto;
 import com.track.track.service.LeetCodeService;
+import com.track.track.service.LastContestService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.*;
 public class LeetCodeController {
 
     private final LeetCodeService leetCodeService;
+    private final LastContestService lastContestService;
 
-    public LeetCodeController(LeetCodeService leetCodeService) {
+    public LeetCodeController(LeetCodeService leetCodeService,
+                               LastContestService lastContestService) {
         this.leetCodeService = leetCodeService;
+        this.lastContestService = lastContestService;
     }
 
     @GetMapping("/{username}")
     public LeetCodeStatsDto getLeetCodeStats(@PathVariable String username) {
-        return leetCodeService.getLeetCodeStats(username);
+        LeetCodeStatsDto dto = leetCodeService.getLeetCodeStats(username);
+        lastContestService.fetchLeetCodeLastContest(username, dto);
+        return dto;
     }
 }
